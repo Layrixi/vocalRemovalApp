e2ea9a6fe9e8a5ac1b96a7d2b97b14fa75a44dab
Layrix <jjacewicz02@gmail.com>
2026-03-04 13:59:19 +0100

    Init

---
+++ main.py
@@ -0,0 +1,10 @@
+import demucs
+
+#uses demucs for now, will create a custom model later on
+class vocalRemovalModel:
+    
+def main():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
