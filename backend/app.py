d3b869f7e43fd3cd8fd9753f766cd06aae85898d
Layrix <jjacewicz02@gmail.com>
2026-03-18 21:48:55 +0100

    removed dev library, added js to load correctly

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
+import os
 
-#debug only, delete later
-from livereload import Server
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -11,12 +11,5 @@ def index():
 
 if __name__ == '__main__':
     # debug=false in prod later, change the port adequatly to the pc(if possible)
-    #debug code
-    server = Server(app.wsgi_app)
-    server.watch('templates/**/*.html')
-    server.watch('static/**/*.css')
-    server.watch('static/**/*.js')
-    server.serve(port=5000, debug=True)
-
     #prod code
-    #app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000)
